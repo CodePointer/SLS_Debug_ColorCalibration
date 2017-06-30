8bff11bd1d1559dfb329192b6643d7c3ae90c02e
Rukun Qiao <1601111330@pku.edu.cn>
2017-06-30 20:02:39 +0800

    Add 2RandDot

--- ColorRegeneration.m
+++ ColorRegeneration.m
@@ -1,6 +1,8 @@
 rand_dot_old = imread('4RandDot0_old.png');
 [PROJECTOR_HEIGHT, PROJECTOR_WIDTH] = size(rand_dot_old);
+rand_dot_old = double(rand_dot_old) / 255.0;
 rand_dot = zeros(PROJECTOR_HEIGHT, PROJECTOR_WIDTH);
+rand_dot_2 = zeros(PROJECTOR_HEIGHT, PROJECTOR_WIDTH);
 
 for h = 1:2:PROJECTOR_HEIGHT
     for w = 1:2:PROJECTOR_WIDTH
@@ -8,6 +10,7 @@ for h = 1:2:PROJECTOR_HEIGHT
     end
 end
 
-rand_dot = rand_dot / 255.0;
+rand_dot_2(rand_dot_old > 0.5) = 1;
 
+imwrite(rand_dot_2, '2RandDot0.png');
 imwrite(rand_dot, '4RandDot0.png');
